47f9f4ae2f2619fa91d44dbf1a4551100ce4088c
RYOTA-F <ry.fj0304@gmail.com>
2023-06-18 21:18:35 +0900

    :recycle: generateStaticParams 削除

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -1,9 +1,7 @@
 import BlogCardList from '@/features/blogs/BlogCardList'
 import TagDetailHeader from '@/features/tags/TagDetailHeader'
-import { MicroCmsBlogUsecase } from '@/usecases/microCMS/blogs/usecaseBlogs.usecase'
-import { MicroCmsTagUsecase } from '@/usecases/microCMS/tags/usecaseTags.usecase'
-import { generateStaticParams } from './generateStaticParams'
 import { generateMetadata } from './generateMetadata'
+import { getPageData } from './page.data'
 
 export interface ITagsPageContext {
   params: {
@@ -12,15 +10,8 @@ export interface ITagsPageContext {
 }
 
 export default async function TagsPage(context: ITagsPageContext) {
-  const microCmsBlogUsecase = new MicroCmsBlogUsecase()
-  const microCmsTagUsecase = new MicroCmsTagUsecase()
-
   const { slug } = context.params
-  const { tag } = await microCmsTagUsecase.getTagByID(slug)
-  const { blogs } = await microCmsBlogUsecase.getBlogs({
-    limit: true,
-    filters: `tags[contains]${slug}`,
-  })
+  const { tag, blogs } = await getPageData(slug)
 
   return (
     <>
@@ -32,4 +23,4 @@ export default async function TagsPage(context: ITagsPageContext) {
   )
 }
 
-export { generateStaticParams, generateMetadata }
+export { generateMetadata }
